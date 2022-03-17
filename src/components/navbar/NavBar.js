e2b5ddb2c8e71c8af6e4243b39eb1ae76a07b034
Ali <ali.galway2002@gmail.com>
2022-03-17 16:18:28 +0000

    Created navbar css file and added some responsivness features for the nav bar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import {useState} from 'react'
 import { Link } from 'react-router-dom'
-
+import {Close, MenuOutlined} from '@material-ui/icons'
+import './NavBar.css';
 
 
 function NavBar () {
 
+    const [active, setActive] = useState(false);
+
+    function showMenu (e) {
+        e.preventDefault()
+        setActive(!active)
+    }
+
     return (
-        <div className="containerNav" >
-            <nav>
+        <div className="navContainer" >
+
+            <div className="menu-icon">
+                <MenuOutlined className="menu" onClick={showMenu} />
+            </div>
+
+            <nav className={active ? 'navBar active' : 'navBar' }>
                 <ul >
+
+                    <div className="closed">
+                        <Close className="close" onClick={showMenu} />
+                    </div>
 
                     <li>
                         <Link to="/" className="navText">Home</Link> 
